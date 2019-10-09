33302826a79c7b828d6277d016eee163b33c7300
Rakesh <n.varma.rakesh@gmail.com>
2019-10-09 16:18:34 -0500

    stlying

--- src/app/instantdecisioncomp/instantdecisioncomp.component.ts
+++ src/app/instantdecisioncomp/instantdecisioncomp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-instantdecisioncomp',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstantdecisioncompComponent implements OnInit {
 
-  constructor() { }
+  showSubContent = 1;
+  constructor( ) {}
+  
+
+  
 
   ngOnInit() {
   }
